641fcaf834dbe51b901612068988fc76afa6d35b
JosyAnjos <josyanjos.souza@gmail.com>
2025-09-07 16:42:36 -0300

    feat: :zap: Adiciona allow_ninl para cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
-  validates_numericality_of :total_price, greater_than_or_equal_to: 0
+  validates_numericality_of :total_price, greater_than_or_equal_to: 0, allow_nil: true
 
   def mark_as_abandoned
     if last_interaction_at.present? && last_interaction_at < 3.hours.ago
