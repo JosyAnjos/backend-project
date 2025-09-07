7815ea4b6344df23f8b10e5d649928ba48764623
JosyAnjos <josyanjos.souza@gmail.com>
2025-09-07 15:33:17 -0300

    feat: :zap: Atualiza métodos com tempos corretos (3h p/ abandonar, 7d p/ remover

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,10 +4,20 @@ class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   def mark_as_abandoned
-    update(abandoned: true) if last_interaction_at < 2.hours.ago
+    if last_interaction_at.present? && last_interaction_at < 3.hours.ago
+      update(abandoned: true)
+    end
   end
 
   def remove_if_abandoned
-    destroy if abandoned? && last_interaction_at < 7.days.ago
+    destroy if abandoned? && last_interaction_at.present? && last_interaction_at < 7.days.ago
+  end
+
+  def abandoned?
+    abandoned
+  end
+
+  def touch_interaction!
+    update!(last_interaction_at: Time.current)
   end
 end
